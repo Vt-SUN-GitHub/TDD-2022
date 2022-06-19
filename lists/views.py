ce4cec7f63caf23cae8dcc4117792326e473b916
Vt-SUN-GitHub <484724845@qq.com>
2022-06-19 20:56:23 +0800

    3 changed files, plus 2 migrations

--- lists/views.py
+++ lists/views.py
@@ -1,19 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 
 def home_page(request):
+    if request.method == 'POST':
+        list_ = List.objects.create()
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
+    # return render(request, 'home.html')
     # items = Item.objects.all()
     # return render(request, 'home.html',{'items':items})
-
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
 
 def new_list(request):
-    Item.objects.create(text = request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text = request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
